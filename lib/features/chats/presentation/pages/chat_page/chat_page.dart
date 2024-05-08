eadd80361f3aada482424f82127605ecfc7a399d
Ahmed Ayman <ahmed.zayan@baianat.com>
2024-05-08 16:40:24 +0300

    fix name overflow in chat page

--- lib/features/chats/presentation/pages/chat_page/chat_page.dart
+++ lib/features/chats/presentation/pages/chat_page/chat_page.dart
@@ -54,91 +54,26 @@ class _ChatPageBodyState extends State<ChatPageBody> {
       listener: (context, state) {},
       builder: (context, state) => Scaffold(
         body: SafeArea(
+          bottom: false,
           child: Column(
             children: [
-              Stack(
-                children: [
-                  const Image(
-                    image: AssetImage(ImagesPaths.chatHeader),
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20.h, left: 10.w),
-                    child: Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: Icon(
-                            Icons.arrow_back_ios,
-                            size: 25.w,
-                            color: Colors.white,
-                          ),
-                        ),
-                        CircleAvatar(
-                            radius: 20,
-                            backgroundImage: widget.user.image != null
-                                ? NetworkImage('${widget.user.image}')
-                                : const NetworkImage(
-                                    'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png')),
-                        SizedBox(
-                          width: 8.w,
-                        ),
-                             SizedBox(
-                               width: MediaQuery.of(context).size.width/3,
-                               height:  MediaQuery.of(context).size.width/10,
-                               child: FittedBox(
-                                child: Text(
-                                  '${widget.user.fullName}',
-                                  maxLines: 1,
-                                  style: TextStyle(
-                                      fontSize: 22.sp,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                      overflow: TextOverflow.ellipsis),
-                                                       ),
-                               ),
-                             ),
-                        const Spacer(),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.phone,
-                            color: Colors.white,
-                            size: 25.w,
-                          ),
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.more_vert_rounded,
-                            size: 25.w,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
               Expanded(
-                child: ConditionalBuilder(
-                  condition: (state.messages?.length != 0),
-                  builder: (context) => Stack(
-                    children: [
-                      Padding(
+                child: Stack(
+                  children: [
+                    ConditionalBuilder(
+                      condition: (state.messages?.length != 0),
+                      builder: (context) => Padding(
                         padding: const EdgeInsets.all(20.0),
                         child: StreamBuilder(
                             stream: state.messages,
                             builder: (context, snapshot) {
                               return ListView.separated(
+                                padding: EdgeInsets.only(bottom: 50.h,top: 70.h),
                                 itemBuilder: (context, index) {
                                   if (UserCubit.get(context)
-                                          .state
-                                          .userData
-                                          ?.firebaseId ==
+                                      .state
+                                      .userData
+                                      ?.firebaseId ==
                                       snapshot.data?[index].senderId) {
                                     return buildMyMessage(
                                         snapshot.data![index]);
@@ -153,111 +88,177 @@ class _ChatPageBodyState extends State<ChatPageBody> {
                               );
                             }),
                       ),
-                    ],
-                  ),
-                  fallback: (context) => const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width - 60,
-                      child: Card(
-                        margin:
-                            const EdgeInsets.only(left: 2, right: 2, bottom: 8),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                        child: TextFormField(
-                          controller: messageController,
-                          textAlignVertical: TextAlignVertical.center,
-                          keyboardType: TextInputType.multiline,
-                          cursorColor: const Color.fromRGBO(70, 121, 112, 1.0),
-                          maxLines: 5,
-                          minLines: 1,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Write a message..',
-                            prefixIcon: IconButton(
-                              color: const Color.fromRGBO(70, 121, 112, 1.0),
-                              icon: const Icon(
-                                Icons.emoji_emotions_outlined,
-                              ),
-                              onPressed: () {},
+                      fallback: (context) => const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                    const Image(
+                      image: AssetImage(ImagesPaths.chatHeader),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.h, left: 10.w),
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: Icon(
+                              Icons.arrow_back_ios,
+                              size: 25.w,
+                              color: Colors.white,
                             ),
-                            suffixIcon: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                  onPressed: () {
-                                    showModalBottomSheet(
-                                        backgroundColor: Colors.transparent,
-                                        context: context,
-                                        builder: (builder) =>
-                                            const SelectAttachmentBottomSheet());
-                                  },
-                                  icon: const Icon(
-                                    Icons.attach_file,
-                                  ),
-                                  color:
-                                      const Color.fromRGBO(70, 121, 112, 1.0),
-                                ),
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                    Icons.camera_alt,
-                                  ),
-                                  color:
-                                      const Color.fromRGBO(70, 121, 112, 1.0),
-                                ),
-                              ],
-                            ),
-                            contentPadding: const EdgeInsets.all(5),
                           ),
-                        ),
+                          CircleAvatar(
+                              radius: 20,
+                              backgroundImage: widget.user.image != null
+                                  ? NetworkImage('${widget.user.image}')
+                                  : const NetworkImage(
+                                      'https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png')),
+                          SizedBox(
+                            width: 8.w,
+                          ),
+                               SizedBox(
+                                 width: MediaQuery.sizeOf(context).width/3,
+                                 child: FittedBox(
+                                   alignment: Alignment.centerLeft,
+                                   fit: BoxFit.scaleDown,
+                                   child: Text(
+                                     '${widget.user.fullName}',
+                                     style: TextStyle(
+                                         fontSize: 22.sp,
+                                         fontWeight: FontWeight.bold,
+                                         color: Colors.white,
+                                         overflow: TextOverflow.ellipsis),
+                                                          ),
+                                 ),
+                               ),
+                          const Spacer(),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.phone,
+                              color: Colors.white,
+                              size: 25.w,
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.more_vert_rounded,
+                              size: 25.w,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: 8,
-                        left: 2,
-                        right: 5,
-                      ),
-                      child: CircleAvatar(
-                        radius: 25,
-                        backgroundColor:
-                            const Color.fromRGBO(70, 121, 112, 1.0),
-                        child: IconButton(
-                          icon: const Icon(
-                            Icons.send_rounded,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {
-                            if (messageController.text.isNotEmpty) {
-                              ChatsCubit.get(context)
-                                  .sendMessage(ApiMessageModel(
-                                senderId: UserCubit.get(context)
-                                        .state
-                                        .userData
-                                        ?.firebaseId ??
-                                    '',
-                                receiverId: widget.user.firebaseId,
-                                dateTime: DateTime.now().toString(),
-                                text: messageController.text,
-                              ));
-                              messageController.clear();
-                            }
-                          },
+                      padding: const EdgeInsets.all(8.0),
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Card(
+                                margin:
+                                const EdgeInsets.only(left: 2, right: 2, bottom: 8),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                child: TextFormField(
+                                  controller: messageController,
+                                  textAlignVertical: TextAlignVertical.center,
+                                  keyboardType: TextInputType.multiline,
+                                  cursorColor: const Color.fromRGBO(70, 121, 112, 1.0),
+                                  maxLines: 5,
+                                  minLines: 1,
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'Write a message..',
+                                    prefixIcon: IconButton(
+                                      color: const Color.fromRGBO(70, 121, 112, 1.0),
+                                      icon: const Icon(
+                                        Icons.emoji_emotions_outlined,
+                                      ),
+                                      onPressed: () {},
+                                    ),
+                                    suffixIcon: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        IconButton(
+                                          onPressed: () {
+                                            showModalBottomSheet(
+                                                backgroundColor: Colors.transparent,
+                                                context: context,
+                                                builder: (builder) =>
+                                                const SelectAttachmentBottomSheet());
+                                          },
+                                          icon: const Icon(
+                                            Icons.attach_file,
+                                          ),
+                                          color:
+                                          const Color.fromRGBO(70, 121, 112, 1.0),
+                                        ),
+                                        IconButton(
+                                          onPressed: () {},
+                                          icon: const Icon(
+                                            Icons.camera_alt,
+                                          ),
+                                          color:
+                                          const Color.fromRGBO(70, 121, 112, 1.0),
+                                        ),
+                                      ],
+                                    ),
+                                    contentPadding: const EdgeInsets.all(5),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                bottom: 8,
+                                left: 2,
+                                right: 5,
+                              ),
+                              child: CircleAvatar(
+                                radius: 25,
+                                backgroundColor:
+                                const Color.fromRGBO(70, 121, 112, 1.0),
+                                child: IconButton(
+                                  icon: const Icon(
+                                    Icons.send_rounded,
+                                    color: Colors.white,
+                                  ),
+                                  onPressed: () {
+                                    if (messageController.text.isNotEmpty) {
+                                      ChatsCubit.get(context)
+                                          .sendMessage(ApiMessageModel(
+                                        senderId: UserCubit.get(context)
+                                            .state
+                                            .userData
+                                            ?.firebaseId ??
+                                            '',
+                                        receiverId: widget.user.firebaseId,
+                                        dateTime: DateTime.now().toString(),
+                                        text: messageController.text,
+                                      ));
+                                      messageController.clear();
+                                    }
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
                   ],
                 ),
               ),
+
             ],
           ),
         ),
@@ -283,7 +284,7 @@ class _ChatPageBodyState extends State<ChatPageBody> {
           child: Column(
             children: [
               (message.image == null || message.image == '')
-                  ? SizedBox.shrink()
+                  ? const SizedBox.shrink()
                   : Image(image: NetworkImage(message.image ?? '')),
               Text(
                 message.text ?? '',
