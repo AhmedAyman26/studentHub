856cdb61e96d7f9b386a3ae13c1373526024dae7
Ahmed Ayman <ahmed.zayan@baianat.com>
2024-05-12 12:39:58 +0300

    - fix app localization - sort posts by date

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graduation/app.dart';
 import 'package:graduation/app_injector.dart';
-import 'package:graduation/bloc_observer.dart';
 import 'package:graduation/common/utils/cache_helper.dart';
 import 'package:graduation/common/utils/dio_helper.dart';
 
@@ -12,7 +10,6 @@ void main() async {
   await initializeDependencies();
   await Firebase.initializeApp();
   await DioHelper.init();
-  Bloc.observer = MyBlocObserver();
   await CacheHelper.init();
 
   runApp(const StudentHubApp());
